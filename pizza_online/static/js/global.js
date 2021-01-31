89ac18037bdc467a57df92a064166093109714ef
Vladimirs Lahiza <vlahiza@rkh.co.uk>
2021-01-31 21:40:28 +0000

    sticky navbar with jquery

--- pizza_online/static/js/global.js
+++ pizza_online/static/js/global.js
@@ -1,12 +1,23 @@
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
+// var header = document.getElementById("myHeader");
+// var sticky = header.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
+
+// Jquery Sticky navbar
+var num = 95 //number of pixels before modifying styles
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+        $('#myHeader').addClass('sticky');
+    } else {
+        $('#myHeader').removeClass('sticky');
+    }
+});
